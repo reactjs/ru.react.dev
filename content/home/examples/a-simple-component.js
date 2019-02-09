c06f04d34204f2574d55f50c6f474418b9106357
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-09 15:26:34 +0300

    Translate sample source code texts

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -2,13 +2,13 @@ class HelloMessage extends React.Component {
   render() {
     return (
       <div>
-        Hello {this.props.name}
+        Привет, {this.props.name}
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="Taylor" />,
+  <HelloMessage name="Иван" />,
   document.getElementById('hello-example')
 );
