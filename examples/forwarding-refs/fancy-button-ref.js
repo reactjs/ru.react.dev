caf32e5039a6fbd8c9a6c7ea06fcf8f6e731fb88
minatko <minatko@gmail.com>
2019-02-13 13:56:10 +0300

    Apply suggestions from code review

--- examples/forwarding-refs/fancy-button-ref.js
+++ examples/forwarding-refs/fancy-button-ref.js
@@ -5,7 +5,7 @@ const ref = React.createRef();
 
 // Компонент FancyButton, который мы импортировали — это HOC LogProps.
 // Несмотря на то, что рендерят они одно и то же,
-// реф в данном случае будт указывать на LogProps, а не на сам FancyButton!
+// реф в данном случае будет указывать на LogProps, а не на сам FancyButton!
 // Это значит, что мы не сможем, например, вызвать ref.current.focus()
 // highlight-range{4}
 <FancyButton
