244eacead04a08a4208a2b9a42e189c8d55e94be
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-28 17:22:33 +0300

    Replace Google Analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,7 +96,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-41298772-1',
+        trackingId: 'UA-130682870-2',
       },
     },
     {
