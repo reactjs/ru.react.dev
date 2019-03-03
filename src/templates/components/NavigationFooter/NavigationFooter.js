99182c01fa2290a676205d221987dd7cd04b932f
Alexey Pyltsyn <lex61rus@gmail.com>
2019-03-03 14:28:28 +0300

    Перевод навигации по страницам

--- src/templates/components/NavigationFooter/NavigationFooter.js
+++ src/templates/components/NavigationFooter/NavigationFooter.js
@@ -40,7 +40,7 @@ const NavigationFooter = ({next, prev, location}) => {
           <Flex basis="50%" type="li">
             {prev && (
               <div>
-                <SecondaryLabel>Previous article</SecondaryLabel>
+                <SecondaryLabel>Предыдущая статья</SecondaryLabel>
                 <div
                   css={{
                     paddingTop: 10,
@@ -61,7 +61,7 @@ const NavigationFooter = ({next, prev, location}) => {
                 textAlign: 'right',
               }}>
               <div>
-                <SecondaryLabel>Next article</SecondaryLabel>
+                <SecondaryLabel>Следующая статья</SecondaryLabel>
                 <div
                   css={{
                     paddingTop: 10,
