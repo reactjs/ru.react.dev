96afd25359d80819a4470f3ead51236371a6cd56
Alexey Pyltsyn <lex61rus@gmail.com>
2019-03-05 04:43:43 +0300

    gatsby-remark-typography: унификация стиля кода

--- plugins/gatsby-remark-typography/index.js
+++ plugins/gatsby-remark-typography/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const visit = require(`unist-util-visit`);
+const visit = require('unist-util-visit');
 const Typograf = require('typograf');
 
 module.exports = ({markdownAST}, pluginOptions = {}) => {
-  visit(markdownAST, `text`, node => {
+  visit(markdownAST, 'text', node => {
     const tp = new Typograf({locale: ['ru']});
     const disabledRules = ['common/space/trimRight', 'common/space/trimLeft'];
 
