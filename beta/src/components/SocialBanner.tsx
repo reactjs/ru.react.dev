4bff770a92fec4a6c92e89c75a479f7512e00093
Roman Zhivotyagin <info@romanzhivo.com>
2022-04-05 19:19:44 +0300

    Social banner has been changed

--- beta/src/components/SocialBanner.tsx
+++ beta/src/components/SocialBanner.tsx
@@ -8,9 +8,9 @@ import {ExternalLink} from './ExternalLink';
 
 // TODO: Unify with the old site settings.
 // Turning this off also requires changing the Page top value to pull up the sidebar.
-const bannerText = 'Поддержите Украину 🇺🇦';
-const bannerLink = 'https://opensource.fb.com/support-ukraine';
-const bannerLinkText = 'Помогите с гуманитарной помощью Украине. Россияне, не молчите! Нет войне!';
+const bannerText = 'Поддержите Россию 🇷🇺';
+const bannerLink = 'https://yandex.ru/search/?lr=213&text=%D1%84%D0%BE%D0%BD%D0%B4%D1%8B+%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D0%B8+%D0%B4%D0%BE%D0%BD%D0%B1%D0%B0%D1%81%D1%81%D1%83';
+const bannerLinkText = 'Помогите с гуманитарной помощью Донбассу. Всем мир!';
 
 export default function SocialBanner() {
   return (
--- src/components/SocialBanner/SocialBanner.js
+++ src/components/SocialBanner/SocialBanner.js
@@ -10,13 +10,13 @@ import React from 'react';
 import {colors, media} from 'theme';
 
 const linkProps = {
-  href: 'https://opensource.fb.com/support-ukraine',
+  href: 'https://yandex.ru/search/?lr=213&text=%D1%84%D0%BE%D0%BD%D0%B4%D1%8B+%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D0%B8+%D0%B4%D0%BE%D0%BD%D0%B1%D0%B0%D1%81%D1%81%D1%83',
   target: '_blank',
   rel: 'noopener',
 };
 
-const bannerText = 'Поддержите Украину 🇺🇦 ';
-const bannerLink = 'Помогите с гуманитарной помощью Украине. Россияне, не молчите! Нет войне!';
+const bannerText = 'Поддержите Россию 🇷🇺 ';
+const bannerLink = 'Помогите с гуманитарной помощью Донбассу. Всем мир!';
 
 export default function SocialBanner() {
   return (
