5f90a9210b7e470b0e523972ef108a70327b00e0
Konstantin Arabei <arabey.k@gmail.com>
2023-05-12 21:39:51 +0700

    translate javascript in jsx with curly braces

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -173,7 +173,7 @@ function YouWillLearn({
   children: any;
   isChapter?: boolean;
 }) {
-  let title = isChapter ? 'In this chapter' : 'You will learn';
+  let title = isChapter ? 'In this chapter' : 'Вы изучите';
   return <SimpleCallout title={title}>{children}</SimpleCallout>;
 }
 
