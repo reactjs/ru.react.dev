c52d22e2c6501ceca8e221461d538fd2da274a4d
zukhrik <kz_spiky@mail.ru>
2023-09-04 11:40:42 +0500

    fixed translation

--- src/components/Layout/HomeContent.js
+++ src/components/Layout/HomeContent.js
@@ -417,7 +417,7 @@ export function HomeContent() {
           <div className="max-w-7xl mx-auto flex flex-col lg:flex-row px-5">
             <div className="max-w-3xl lg:max-w-7xl gap-5 flex flex-col lg:flex-row lg:px-5">
               <div className="w-full lg:w-6/12 max-w-3xl flex flex-col items-start justify-start lg:ps-5 lg:pe-10">
-                <Header>бновите, когда будущее будет готово</Header>
+                <Header>Обновляйтесь, когда будущее будет готово</Header>
                 <Para>
                   React подходит к изменениям с осторожностью. Каждый коммит
                   React проверено на критически важных для бизнеса поверхностях
